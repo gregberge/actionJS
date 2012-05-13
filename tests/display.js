df729fdf6a5cfc209f4d1293ce2c9d00fbbeeb63
Greg Bergé <neoziro@hotmail.com>
2012-05-13 22:31:45 +0200

    DisplayObject code + tests

--- tests/display.js
+++ tests/display.js
@@ -3,14 +3,47 @@ define(["jquery", "qunit"], function($, qunit)
    return {
       start : function()
       {
-         require(["aj/display/DisplayObject"], function(DisplayObject)
+         require(["aj/display/DisplayObject", "aj/geom/Point", "aj/geom/Rectangle"],
+         function(DisplayObject, Point, Rectangle)
          {
             module("display");
             
             test("aj/display/DisplayObject", function()
             {
                var displayObject = new DisplayObject();
+               displayObject.width = 20;
+               displayObject.height = 50;
+               
                ok(displayObject instanceof DisplayObject, "instanciate");
+               
+               equal(displayObject.localToGlobal(new Point(0, 0)).x, 0, "localToGlobal() x=0");
+               equal(displayObject.localToGlobal(new Point(0, 0)).y, 0, "localToGlobal() y=0");
+               
+               displayObject.x = 5;
+               displayObject.y = 10;
+               
+               equal(displayObject.localToGlobal(new Point(5, 10)).x, 5, "localToGlobal() x=5");
+               equal(displayObject.localToGlobal(new Point(5, 10)).y, 10, "localToGlobal() y=10");
+               
+               ok(displayObject.getRect(displayObject).equals(new Rectangle(5, 10, 0, 0)), "getRect()");
+               
+               return ;
+               equal(displayObject.globalToLocal(new Point(5, 10)).x, 5, "globalToLocal() x=5");
+               equal(displayObject.globalToLocal(new Point(5, 10)).y, 10, "globalToLocal() y=10");
+               
+               ok(displayObject.hitTest(new Point(10, 15)), "hitTest(point) ok");
+               equal(displayObject.hitTest(new Point(100, 15)), false, "hitTest(point) nok");
+               
+               var displayObject2 = new DisplayObject();
+               displayObject2.width = 2;
+               displayObject2.height = 2;
+               displayObject2.x = 24;
+               displayObject2.y = 59;
+               
+               ok(displayObject2.hitTest(displayObject), "hitTest(displayObject) ok");
+               
+               displayObject.y = 90;
+               equal(displayObject2.hitTest(displayObject), false, "hitTest(displayObject) nok");
             });
          });
          
